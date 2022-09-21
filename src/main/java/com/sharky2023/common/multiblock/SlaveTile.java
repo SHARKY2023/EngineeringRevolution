e83a2ad7ed760de31d40b06ed10a196c22f6d5e8
Zac <zacroyle@gmail.com>
2022-09-22 02:12:37 +1000

    new things

--- src/main/java/com/sharky2023/common/multiblock/SlaveTile.java
+++ src/main/java/com/sharky2023/common/multiblock/SlaveTile.java
@@ -2,39 +2,48 @@ package com.sharky2023.common.multiblock;
 
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.registries.ForgeRegistries;
 
 
 import javax.annotation.Nullable;
 
 public class SlaveTile extends BlockEntity implements ISlaveLogic {
 
-
-    @Nullable
-    private BlockPos masterPos;
-    @Nullable
-    private Block masterBlock;
-
     public SlaveTile(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
     }
 
 
-    public boolean hasMaster(){
-        return masterPos != null;
+    @Nullable
+    @Override
+    public BlockPos getMasterPos() {
+        return null;
     }
 
-    protected void setMaster(@Nullable BlockPos master, @Nullable Block block) {
-        masterPos = master;
-        masterBlock = block;
-        this.setChangedFast();
+    @Override
+    public void notifyMasterOfChange(BlockPos Pos, BlockState state) {
+
     }
 
+    @Override
+    public boolean isValidMaster(IMasterLogic master) {
+        return false;
+    }
 
+    @Override
+    public void setPotentialMaster(IMasterLogic master) {
 
+    }
 
+    @Override
+    public void removeMaster(IMasterLogic master) {
 
+    }
 }
